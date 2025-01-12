888d0299f75b984380db8aa8d4be445d058ace66
spedatox <112463481+spedatox@users.noreply.github.com>
2025-01-12 21:52:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,7 +129,7 @@ def generate_response(user_input, user_name, messages):
     if not user_input:
         return "No user input provided."
 
-    content = f"Adın Speda Ahmet Erol Bayrak Tarafından Geliştirilen Bir Yapay Zekasın. Kod yazabilir, metin oluşturabilir, bir yapay zeka asistanının yapabildiği neredeyse herşeyi yapabilirsin."
+    content = f"Senin adın Speda. Ahmet Erol Bayrak Tarafından Geliştirilen Bir Yapay Zekasın. Kod yazabilir, metin oluşturabilir, bir yapay zeka asistanının yapabildiği neredeyse herşeyi yapabilirsin. Kullanıcının adı {user_name}"
     prompt = f"{content}\n\n{user_input}"
 
     client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
